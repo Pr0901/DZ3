934add59b32518d0f5c806ae09086a2cce9f0030
Sofya <sofa33158@gmail.com>
2023-01-08 12:24:30 +0300

    DZ3

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
-        int eaters = 5; // сколько людей будут есть
+        int initial_account = 100;
+        int payment = 1200;
+        int bonus;
+        if (payment>1000) {
+            bonus = payment/100;
+        } else {
+            bonus = 0;
+        }
+        int final_account = initial_account + payment + bonus;
 
-        int water = 3000; // миллилитров воды
-        int potatoes = 5; // картофелин
-        int chicken = 6; // куриных бёдер
-        int spices = 10; // ложек специй
+        System.out.println("На счету:" + final_account);
 
-        System.out.println("Сварили суп. На одного человека вышло:");
-        System.out.println((water / eaters) + " миллилитров воды");
-        System.out.println((potatoes / eaters) + " картофелин(а)");
-        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
-        System.out.println((spices / eaters) + " ложек(ка) специй");
-        
 
     }
 }
